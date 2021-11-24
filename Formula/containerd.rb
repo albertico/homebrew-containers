ad86ca10dc8b8cad673218b8a529417445852bb9
Alberto Colón Viera <aacv@alberti.co>
2021-11-23 23:28:42 -0500

    Adding git revision to formula and installing man pages

--- Formula/containerd.rb
+++ Formula/containerd.rb
@@ -1,9 +1,10 @@
 class Containerd < Formula
-  desc "An open and reliable container runtime"
+  desc "Open and reliable container runtime"
   homepage "https://containerd.io"
   url "https://github.com/containerd/containerd",
-    using:  :git,
-    tag:    "v1.5.8"
+    using:    :git,
+    tag:      "v1.5.8",
+    revision: "1e5ef943eb76627a6d3b6de8cd1ef6537f393a71"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -11,7 +12,10 @@ class Containerd < Formula
 
   def install
     system "make"
+    system "make", "genman"
     bin.install Dir["bin/*"]
+    man.mkpath
+    man8.install Dir["man/*.8"]
   end
 
   test do
